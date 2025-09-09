c58c0a94d0daf08cb20943e7b5b37ff532b47696
Ralph Abdallah <ralphsaidabdallah@gmail.com>
2025-09-09 15:30:46 +0300

    fix icon bug vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/todo-app/' : '/', // dev = '/', prod = '/todo-app/'
+  base: command === 'build' ? '/' : '/', // dev = '/', prod = '/todo-app/'
 }))
